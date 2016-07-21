30a1e823d19d58733b1eeb7c169b8c4056875c26
Hyeokjun <ann10624@naver.com>
2016-07-21 13:13:32 +0900

    레이아웃(일부코드적용_수정중)

--- app/src/main/java/com/example/user/write/cost_config.java
+++ app/src/main/java/com/example/user/write/cost_config.java
@@ -1,12 +1,22 @@
 package com.example.user.write;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class cost_config extends Activity{
+public class cost_config extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub1);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+        toolbar.setTitle(R.string.text_login);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
--- app/src/main/java/com/example/user/write/cost_request.java
+++ app/src/main/java/com/example/user/write/cost_request.java
@@ -1,12 +1,16 @@
 package com.example.user.write;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
-public class cost_request extends Activity{
+public class cost_request extends AppCompatActivity{
     private Context CONTEXT;
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,7 +19,24 @@ public class cost_request extends Activity{
         setContentView(R.layout.activity_sin);
         CONTEXT= this;
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+        toolbar.setTitle(R.string.text_login);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                finish();
+        }
+        return (super.onOptionsItemSelected(menuItem));
+    }
+
     public void onClick(View view)
     {
         EditText txt_1 = (EditText) findViewById(R.id.name_sa);
