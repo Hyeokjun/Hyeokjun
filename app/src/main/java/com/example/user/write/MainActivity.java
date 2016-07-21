1b820fa62c9beabca2f9019d4b3e53d69c003745
Hyeokjun <ann10624@naver.com>
2016-07-21 13:19:29 +0900

    레이아웃(일부코드적용_수정중)

--- app/src/main/java/com/example/user/write/MainActivity.java
+++ app/src/main/java/com/example/user/write/MainActivity.java
@@ -42,12 +42,13 @@ public class MainActivity extends AppCompatActivity {
         EditText txt_pwd = (EditText) findViewById(R.id.pwd);
         Intent intent = new Intent(this,Cost.class);
         if(txt_id.equals(null)){
-            Toast.makeText(CONTEXT, "입력받은 id 가 없습니다.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(CONTEXT, txt_id.getText(), Toast.LENGTH_SHORT).show();
         }
         else if(txt_pwd.equals(null)){
             Toast.makeText(CONTEXT, "입력받은 pwd 가 없습니다.", Toast.LENGTH_SHORT).show();
         }
         else{
+            Toast.makeText(CONTEXT, txt_id.getText(), Toast.LENGTH_SHORT).show();
             startActivity(intent);
         }
     }
